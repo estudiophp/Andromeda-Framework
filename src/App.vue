e40f0badf4580f720030480a36a28297b67782ff
Christian Fernando Paredes <christianfernando16@outlook.com>
2021-08-12 09:14:17 -0500

    Color themes

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
+  <div></div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+
 export default defineComponent({
-  name: 'App',
+  name: 'App'
 })
 </script>
