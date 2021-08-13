eea36b50716ec4d32b2bd1499c9b079b70e3a77c
Christian Paredes Rivera <christian.fernando.paredes@gmail.com>
2021-08-12 19:52:07 -0500

    Spaces: Margin and Padding

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <div></div>
+  <div class="background-primary margin-top-extra">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis doloremque dolores harum hic, nihil quis similique soluta veritatis! Architecto asperiores cupiditate debitis ea ipsa libero optio repellat saepe soluta voluptatem!
+  </div>
 </template>
 
 <script lang="ts">
