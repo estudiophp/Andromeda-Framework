54b2650bea3509b5d694106299556feb2e6dd5fc
Christian Paredes Rivera <christian.fernando.paredes@gmail.com>
2021-09-08 10:51:36 -0500

    Component: Card and Card section

---
+++ src/controllers/Card/Card.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card" :class="{'block': block}">
+    <slot/>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/runtime-core";
+
+export default defineComponent({
+  name: 'Card',
+  props: {
+    block: {
+      default: false,
+      type: Boolean
+    }
+  }
+});
+</script>
+
+<style>
+</style>
