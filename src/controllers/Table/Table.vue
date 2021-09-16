e27dcbf41d60de818bf13e0df10dbd0ef36ec9ee
Christian Paredes Rivera <christian.fernando.paredes@gmail.com>
2021-09-16 13:55:37 -0500

    feat: add component Table

---
+++ src/controllers/Table/Table.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="table-responsive">
+    <table class="table">
+      <thead>
+        <slot name="head"/>
+      </thead>
+      <tbody>
+        <slot name="body"/>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: 'Table'
+})
+</script>
